2b174f8b861cd565c9c2abedc1b28afbe7dae975
Facundo Soria <facundo.cba2@gmail.com>
2023-03-18 19:10:20 -0300

    dia 1

---
+++ HelloWord/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	const pi float64 = 3.14
+	const pi2 = 3.15
+
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
+
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println("Base:", base)
+	fmt.Println("Altura:", altura)
+	fmt.Println("Area:", area)
+}
